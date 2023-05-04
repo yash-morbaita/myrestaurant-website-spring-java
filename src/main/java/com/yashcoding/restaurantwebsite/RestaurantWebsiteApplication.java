5d500b11c02a8055097b7b25491a567ab168c125
Yes <wanderer@Yess-MacBook-Air.local>
2023-05-04 14:28:19 -0400

    Files Upload

---
+++ src/main/java/com/yashcoding/restaurantwebsite/RestaurantWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.yashcoding.restaurantwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yashcoding/restaurantwebsite/RestaurantWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashcoding.restaurantwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
